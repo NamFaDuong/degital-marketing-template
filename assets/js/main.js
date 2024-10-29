ddd202ba484138cd742fcb3761b3a558b1d69450
NamfaDuong <namfaduong2289@gmail.com>
2024-10-29 15:08:28 +0700

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,50 @@
+/*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+    navToggle = document.getElementById('nav-toggle'),
+    navClose = document.getElementById('nav-close')
+
+/* Menu show */
+if (navToggle) {
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu')
+    })
+}
+
+/* Menu hidden */
+if (navClose) {
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu')
+    })
+}
+/*=============== ADD BLUR TO HEADER ===============*/
+const blurHeader = () => {
+    const header = document.getElementById('header')
+    // Add a class if the bottom offset is greater than 50 of the viewport, add blur-header class
+    this.scrollY >= 50 ? header.classList.add('blur-header')
+        : header.classList.remove('blur-header')
+}
+window.addEventListener('scroll', blurHeader)
+
+/*=============== SHOW SCROLL UP ===============*/
+const scrollUp = () => {
+    const scrollUp = document.getElementById('scroll-up')
+    // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup class
+    this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+        : scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
+
+document.addEventListener('keydown', function (event) {
+    // Prevent Windows Print Screen
+    if (event.key === 'PrintScreen') {
+        alert('Screenshots are disabled on this website.');
+        event.preventDefault();
+    }
+
+    // Prevent Windows Snipping Tool (Windows + Shift + S)
+    if (event.ctrlKey && event.shiftKey && event.key === 's') {
+        alert('Screenshots are disabled on this website.');
+        event.preventDefault();
+    }
+
+});
